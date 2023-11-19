46edfc09af5b025b617d02d74ef38bad934a7bcd
quannhg <quan.nguyenhg12@gmail.com>
2023-11-19 23:48:07 +0700

    feat(user): update user API with new database handler functions for PostgreSQL
    Integrate new database handler functions of PostgreSQL into the user API.

--- src/queries/employee.ts
+++ src/queries/employee.ts
@@ -21,4 +21,24 @@ const selectByUsername = async (username: string, fields?: string[]): Promise<Re
     }
 };
 
-export const employeeQuery = { selectByUsername };
+const selectById = async (id: string, fields?: string[]): Promise<Record<string, string>[]> => {
+    try {
+        // Generate the SELECT query dynamically based on the provided fields or default to '*'
+        const selectFields = fields && fields.length > 0 ? fields.join(', ') : '*';
+        const queryText = `SELECT ${selectFields} FROM nhan_vien WHERE ma_nhan_vien = $1`;
+
+        const poolClient = createPoolClient();
+
+        await poolClient.connect();
+
+        const { rows } = await poolQuery({ text: queryText, values: [id] });
+
+        return rows;
+    } catch (err) {
+        logger.error('Error when retrieving user data by Ma_nhan_vien');
+        logger.error(err);
+        throw err;
+    }
+};
+
+export const employeeQuery = { selectByUsername, selectById };
