ffdc58898a203d659e45a7db7a585fe04153b847
Qmi <minh.pham1126@gmail.com>
2023-12-05 11:52:19 +0700

    feat(mockdata): don_hang_ap_dung_khuyen_mai & don_hang_gom_mon

---
+++ database/data/don_hang_ap_dung_khuyen_mai.sql
@@ -0,0 +1,7 @@
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('68786-220', '41163-917');
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('37808-552', '53964-002');
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('41163-742', '43063-088');
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('68084-523', '68788-0633');
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('68786-220', '41163-917');
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('37808-552', '53964-002');
+insert into don_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_don_hang) values ('41163-742', '43063-088'); 
