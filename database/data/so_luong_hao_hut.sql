25084d40154144daa937e97942c3c9ead5f54cbe
Qmi <minh.pham1126@gmail.com>
2023-12-06 20:03:49 +0700

    feat(mockdata): so_luong_hao_hut

---
+++ database/data/so_luong_hao_hut.sql
@@ -0,0 +1,16 @@
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (7, '11/11/2022', '307ac585-7af5-4642-80b2-99726495a060', '52125-215');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (2, '11/7/2022', '307ac585-7af5-4642-80b2-99726495a060', '0781-3428');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (5, '11/19/2022', '307ac585-7af5-4642-80b2-99726495a060', '10337-163');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (2, '12/4/2022', '307ac585-7af5-4642-80b2-99726495a060', '51531-6743');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (4, '12/4/2022', '307ac585-7af5-4642-80b2-99726495a060', '51531-6743');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (3, '11/15/2022', '307ac585-7af5-4642-80b2-99726495a060', '0135-0140');
+
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (2, '12/4/2022', '79f44967-7044-48f1-8b24-69386cc1b50a', '52125-215');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (5, '12/5/2022', '79f44967-7044-48f1-8b24-69386cc1b50a', '33261-484');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (6, '12/4/2022', '79f44967-7044-48f1-8b24-69386cc1b50a', '52125-215');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (1, '11/25/2022', '79f44967-7044-48f1-8b24-69386cc1b50a', '52125-377');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (3, '11/29/2022', '79f44967-7044-48f1-8b24-69386cc1b50a', '52125-377');
+
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (5, '12/3/2022', 'd40c35a7-f8fb-4eee-bb00-8f67fc6d4fb7', '63402-206');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (9, '11/6/2022', 'd40c35a7-f8fb-4eee-bb00-8f67fc6d4fb7', '54868-4277');
+insert into so_luong_hao_hut (So_luong_hao_hut, Ngay_hao_hut, Ma_cua_hang, Ma_nguyen_lieu) values (6, '12/2/2022', 'd40c35a7-f8fb-4eee-bb00-8f67fc6d4fb7', '10337-163');
