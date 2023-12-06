82c3f611a263d31056096ba0d38f1820e11b6ddf
Qmi <minh.pham1126@gmail.com>
2023-12-06 20:02:42 +0700

    feat(mockdata): cua_hang_ap_dung_khuyen_mai

---
+++ database/data/cua_hang_ap_dung_khuyen_mai.sql
@@ -0,0 +1,3 @@
+insert into cua_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_cua_hang) values ('63629-3760', '79f44967-7044-48f1-8b24-69386cc1b50a');
+insert into cua_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_cua_hang) values ('64117-178', 'd40c35a7-f8fb-4eee-bb00-8f67fc6d4fb7');
+insert into cua_hang_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_cua_hang) values ('42388-012', '79f44967-7044-48f1-8b24-69386cc1b50a');
