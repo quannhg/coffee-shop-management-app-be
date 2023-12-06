dd640d0dc850ea9dcf409ade7cd9b7d0f49dee04
Qmi <minh.pham1126@gmail.com>
2023-12-06 20:03:36 +0700

    feat(mockdata): phat_thuong

---
+++ database/data/phat_thuong.sql
@@ -0,0 +1,13 @@
+insert into phat (Ma_nhan_vien, Thoi_gian_phat, Tien_phat, Ly_do_phat) values ('a39f4ae9-86fc-44f9-afb1-0d1ae7c3a7ce', TO_DATE('2022-11-29','YYYY-MM-DD'), 83148, 'abc');
+insert into phat (Ma_nhan_vien, Thoi_gian_phat, Tien_phat, Ly_do_phat) values ('0c65484e-adac-4ae5-8b89-ca36c0ea23cb', TO_DATE('2022-11-26','YYYY-MM-DD'), 69391, 'xyz');
+insert into phat (Ma_nhan_vien, Thoi_gian_phat, Tien_phat, Ly_do_phat) values ('4c8d18e0-267d-43b9-9775-375b5f1d2fe5', TO_DATE('2022-11-14','YYYY-MM-DD'), 54630, 'mnop');
+insert into phat (Ma_nhan_vien, Thoi_gian_phat, Tien_phat, Ly_do_phat) values ('44181f87-9211-43ad-953d-500d67333707', TO_DATE('2022-11-29','YYYY-MM-DD'), 54237, 'qwerty');
+insert into phat (Ma_nhan_vien, Thoi_gian_phat, Tien_phat, Ly_do_phat) values ('37e46bad-b103-4701-885c-65f677030cff', TO_DATE('2022-11-26','YYYY-MM-DD'), 87138, 'ilu');
+insert into phat (Ma_nhan_vien, Thoi_gian_phat, Tien_phat, Ly_do_phat) values ('4cbc79cd-e38e-47d8-9e9f-1fd1cf776923', TO_DATE('2022-11-27','YYYY-MM-DD'), 55653, 'anhyeuem');
+
+insert into thuong (Ma_nhan_vien, Thoi_gian_thuong, Tien_thuong, Ly_do_thuong) values ('4c8d18e0-267d-43b9-9775-375b5f1d2fe5', TO_DATE('2022-11-10','YYYY-MM-DD'), 57905, 'abc');
+insert into thuong (Ma_nhan_vien, Thoi_gian_thuong, Tien_thuong, Ly_do_thuong) values ('4cbc79cd-e38e-47d8-9e9f-1fd1cf776923', TO_DATE('2022-11-09','YYYY-MM-DD'), 91315, 'xyz');
+insert into thuong (Ma_nhan_vien, Thoi_gian_thuong, Tien_thuong, Ly_do_thuong) values ('0cdeaa63-3b34-4a34-bf34-5d9b77b7f9e2', TO_DATE('2022-11-17','YYYY-MM-DD'), 80500, 'mnop');
+insert into thuong (Ma_nhan_vien, Thoi_gian_thuong, Tien_thuong, Ly_do_thuong) values ('0ee57da6-c54b-4f05-9379-5baad1ba7faa', TO_DATE('2022-11-16','YYYY-MM-DD'), 89589, 'qwerty');
+insert into thuong (Ma_nhan_vien, Thoi_gian_thuong, Tien_thuong, Ly_do_thuong) values ('240a0fab-6c8e-40ab-8980-e9921f8b0b81', TO_DATE('2022-11-10','YYYY-MM-DD'), 79733, 'ilu');
+insert into thuong (Ma_nhan_vien, Thoi_gian_thuong, Tien_thuong, Ly_do_thuong) values ('e62309e6-5d39-4fb2-bbf0-e9b4b05bb879', TO_DATE('2022-11-17','YYYY-MM-DD'), 71974, 'anhyeuem');
