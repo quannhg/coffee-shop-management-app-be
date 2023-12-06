45970bef8a961117d17d172a84af581746c53d01
Qmi <minh.pham1126@gmail.com>
2023-12-06 20:03:20 +0700

    feat(mockdata): mon_ap_dung_khuyen_mai

---
+++ database/data/mon_ap_dung_khuyen_mai.sql
@@ -0,0 +1,4 @@
+insert into mon_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_mon) values ('42507-692', '52686-276');
+insert into mon_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_mon) values ('49288-0004', '54868-6223');
+insert into mon_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_mon) values ('68382-228', '51346-081');
+insert into mon_ap_dung_khuyen_mai (Ma_khuyen_mai, Ma_mon) values ('63629-3760', '37808-773');
