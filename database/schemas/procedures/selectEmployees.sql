6f817ad73c8b03661acbb7ad4a1fb0c560f162c1
quannhg <quan.nguyenhg12@gmail.com>
2023-12-07 13:10:59 +0700

    fix(check_du_nguyen_lieu): correct mon_id type from uuid to varchar(30)
    addtionally, comment uncorrect function selectEmployeeByStore with ERROR tag

--- database/schemas/procedures/selectEmployees.sql
+++ database/schemas/procedures/selectEmployees.sql
@@ -1,20 +1,18 @@
--- thủ tục select nhân viên của cửa hàng chỉ định sắp xếp theo tên
-
-CREATE OR REPLACE PROCEDURE selectEmployeeByStore(maCuaHang uuid,INOUT result refcursor)
-LANGUAGE 'plpgsql'
-AS $BODY$         
-BEGIN
-open result for 
-    SELECT  nv.* FROM nhan_vien AS nv
-    JOIN nhan_vien_lam_viec_tai_cua_hang AS nvlvtch ON nv.ma_nhan_vien = nvlvtch.ma_nhan_vien
-    JOIN cua_hang AS ch ON nvlvtch.ma_cua_hang = ch.ma_cua_hang
-    WHERE ch.ma_cua_hang = maCuaHang
-    ORDER BY nv.ho_va_ten;
-END;
-$BODY$;
-
-BEGIN;
-CALL selectEmployeeByStore( '',  'test');
-fetch all in "test";
-COMMIT;
-
+--ERROR: invalid input syntax for type uuid: ""
+-- -- thủ tục select nhân viên của cửa hàng chỉ định sắp xếp theo tên
+ -- CREATE OR REPLACE PROCEDURE selectEmployeeByStore(maCuaHang uuid,INOUT result refcursor)
+-- LANGUAGE 'plpgsql'
+-- AS $BODY$
+-- BEGIN
+-- open result for
+--     SELECT  nv.* FROM nhan_vien AS nv
+--     JOIN nhan_vien_lam_viec_tai_cua_hang AS nvlvtch ON nv.ma_nhan_vien = nvlvtch.ma_nhan_vien
+--     JOIN cua_hang AS ch ON nvlvtch.ma_cua_hang = ch.ma_cua_hang
+--     WHERE ch.ma_cua_hang = maCuaHang
+--     ORDER BY nv.ho_va_ten;
+-- END;
+-- $BODY$;
+ -- BEGIN;
+-- CALL selectEmployeeByStore( '',  'test');
+-- fetch all in "test";
+-- COMMIT;
