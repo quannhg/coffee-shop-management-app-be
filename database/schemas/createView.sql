466a46fe2a8e5eb622be1d08f651095484fb3172
Qmi <minh.pham1126@gmail.com>
2023-12-07 15:16:12 +0700

    fix(view): replace v_SL_nguyen_lieu_cuahang with v_cuahang_chua_soluong_nguyenlieu

--- database/schemas/createView.sql
+++ database/schemas/createView.sql
@@ -1,25 +1,52 @@
 --create view in here
 
-CREATE VIEW v_SL_nguyen_lieu_cua_hang AS
-SELECT
-    ch.Ma_cua_hang,
-    ch.Ten_cua_hang,
-    nl.Ma_nguyen_lieu,
-    nl.Ten_nguyen_lieu,
-    nl.Don_vi,
-    COUNT(chnl.Ma_nguyen_lieu) AS So_luong
+CREATE VIEW v_cuahang_chua_soluong_nguyenlieu AS
+SELECT 
+    COALESCE(sl_in.ma_cua_hang, sl_out.ma_cua_hang, sl_haohut.ma_cua_hang) as ma_cua_hang,
+    COALESCE(sl_in.ma_nguyen_lieu, sl_out.ma_nguyen_lieu, sl_haohut.ma_nguyen_lieu) as ma_nguyen_lieu,
+    (COALESCE(sl_in.so_luong, 0) - COALESCE(sl_out.so_luong,0) - COALESCE(sl_haohut.so_luong, 0)) as so_luong
 FROM
-    CUA_HANG ch
-JOIN
-    CUA_HANG_CHUA_NGUYEN_LIEU chnl ON ch.Ma_cua_hang = chnl.Ma_cua_hang
-JOIN
-    NGUYEN_LIEU nl ON chnl.Ma_nguyen_lieu = nl.Ma_nguyen_lieu
-GROUP BY
-    ch.Ma_cua_hang,
-    ch.Ten_cua_hang,
-    nl.Ma_nguyen_lieu,
-    nl.Ten_nguyen_lieu,
-    nl.Don_vi;
+(
+    select 
+        hd_nl.ma_nguyen_lieu,
+        hd.ma_cua_hang,
+        sum(hd_nl.so_luong) as so_luong
+    from
+        hoa_don_nhap_kho hd
+    JOIN
+        hoa_don_nhap_kho_co_nguyen_lieu hd_nl on hd.ma_hoa_don = hd_nl.ma_hoa_don
+    GROUP BY
+        ma_cua_hang,
+        ma_nguyen_lieu
+) sl_in
+FULL JOIN
+(
+    select 
+        dh.ma_cua_hang,
+        m.ma_nguyen_lieu,
+        sum(dhm.so_luong * m.so_luong) as so_luong
+    from
+        don_hang_gom_mon dhm
+    join 
+        mon_can_nguyen_lieu m on dhm.ma_mon = m.ma_mon 
+    join 
+        don_hang dh on dh.ma_don_hang = dhm.ma_don_hang
+    GROUP BY
+        dh.ma_cua_hang,
+        m.ma_nguyen_lieu
+) sl_out ON sl_in.ma_cua_hang = sl_out.ma_cua_hang AND sl_in.ma_nguyen_lieu = sl_out.ma_nguyen_lieu
+FULL JOIN
+(
+    select 
+        sum(so_luong_hao_hut) as so_luong,
+        ma_cua_hang,
+        ma_nguyen_lieu
+    from 
+        so_luong_hao_hut
+group by 
+        ma_cua_hang,
+        ma_nguyen_lieu
+) sl_haohut ON COALESCE(sl_in.ma_cua_hang, sl_out.ma_cua_hang) = sl_haohut.ma_cua_hang AND COALESCE(sl_in.ma_nguyen_lieu, sl_out.ma_nguyen_lieu) = sl_haohut.ma_nguyen_lieu;
 
 
 CREATE VIEW v_So_ban_TRONG AS
@@ -31,6 +58,3 @@ CREATE VIEW v_Tong_tien_MON AS
 SELECT dh.Ma_don_hang, m.Ten_mon, dhm.So_luong * m.Gia_tien AS Tong_tien
 FROM DON_HANG_GOM_MON dhm JOIN MON m ON dhm.Ma_mon = m.Ma_mon
 JOIN DON_HANG dh ON dhm.Ma_don_hang = dh.Ma_don_hang;
-
-
-
